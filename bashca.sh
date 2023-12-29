4d1033d68800f2505171c0f056db23e4101d3429
Hackthical <111184438+Hackthical@users.noreply.github.com>
2023-12-29 12:56:33 +0330

    Update bashca.sh

--- bashca.sh
+++ bashca.sh
@@ -37,6 +37,17 @@ services:
     volumes:
       - "/docker/appdata/firefox-hadi:/config:rw"
     restart: always
+  squid:
+    image: cooolin/socks5
+    container_name: socks5
+    ports:
+      - "12334:1080"
+    environment:
+      - PROXY_HOST=0.0.0.0
+      - PROXY_PORT=1080
+#     - PROXY_USER=cooolin
+#     - PROXY_PASS=password
+    restart: always
 EOF
 sudo docker-compose up -d
 sudo ufw default allow routed
